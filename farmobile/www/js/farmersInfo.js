ac1f3e4794accc8ab2c66913b1b0f7b2fe264642
ענבר חאלו <inbarhallo@gmail.com>
2017-08-10 23:43:26 +0300

    1.0

---
+++ farmobile/www/js/farmersInfo.js
@@ -0,0 +1,64 @@
+$(function(){
+    $.ajax({
+        url: "http://Vmedu122.mtacloud.co.il:8080/APPserver/clientServlet",
+        data: {requestType :"getFarmersInfo"},
+        success: function(information) {
+            console.log(information);
+            for(var i=0; i<information.length; i++) {
+                var id = information[i].split("-");
+                var container = document.createElement('div');
+                container.className = "contain col-xs-5";
+                var img = document.createElement('img');
+                img.src = "img/farmer.png";
+                img.id = id[1];
+                img.onclick = function() { window.location.href = "personalFarmerInfo.html"+ "?id=" + this.id; };
+                var text = document.createElement('span');
+                text.innerText = id[0];
+                text.className = "label label-info";
+                text.id = "text";
+                container.appendChild(img);
+                container.appendChild(text);
+                document.body.appendChild(container);
+            }
+        },
+        error: function(lo){   console.log("error " + lo.message);}
+    });
+    var centerControlDiv = document.createElement('button');
+    centerControlDiv.className="btn-block";
+    centerControlDiv.id="upperButton";
+    centerControlDiv.disabled = true;
+
+    var menuButton = document.createElement('button');
+    menuButton.id = "menu";
+    menuButton.addEventListener('click', function() {
+        openNav();
+    });
+    centerControlDiv.appendChild(menuButton);
+
+    var rightLogo = document.createElement('button');
+    rightLogo.disabled = true;
+    rightLogo.id = "rightLogo";
+    centerControlDiv.appendChild(rightLogo);
+    centerControlDiv.index = 1;
+    document.body.appendChild(centerControlDiv);
+
+    setText();
+});
+
+function searchFarmer(){
+    var found = 0;
+    var text = document.getElementById('search').value;
+    var x = document.getElementsByClassName("contain col-xs-5");
+    var toRemove = []
+    for (var i=0; i< x.length; i++){
+        if(x[i].childNodes[1].textContent != text)
+            toRemove.push(x[i]);
+        else
+            found = 1;
+    }
+    var size = toRemove.length;
+    for(i=0; i<size; i++)
+        document.body.removeChild(toRemove.pop());
+    if (found == 0)
+        document.getElementById('dataMissing').innerText = ".לא נמצאו תוצאות";
+}
